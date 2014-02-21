fb05e182b5086b17be4ec9c9c489a6630d6e369c
Markus Kauppila <markus.kauppila@gmail.com>
2014-02-21 20:10:26 +0200

    Add HelmetAPIClient
    Encapsulates the communication between the app and
    the library servers.

---
+++ Helmi/HelmetAPIClient.h
@@ -0,0 +1,13 @@
+//
+//  HelmetAPIClient.h
+//  Helmi
+//
+//  Created by markus on 21.2.2014.
+//  Copyright (c) 2014 Markus Kauppila. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HelmetAPIClient : NSObject
+
+@end
---
+++ Helmi/HelmetAPIClient.m
@@ -0,0 +1,13 @@
+//
+//  HelmetAPIClient.m
+//  Helmi
+//
+//  Created by markus on 21.2.2014.
+//  Copyright (c) 2014 Markus Kauppila. All rights reserved.
+//
+
+#import "HelmetAPIClient.h"
+
+@implementation HelmetAPIClient
+
+@end
