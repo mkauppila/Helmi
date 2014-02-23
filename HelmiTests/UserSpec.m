e243a65f4cac56f2afc08bb0ff4da45d06072844
Markus Kauppila <markus.kauppila@gmail.com>
2014-02-23 17:22:20 +0200

    Add UserSpec

---
+++ HelmiTests/UserSpec.m
@@ -0,0 +1,21 @@
+//
+//  HelmiTests.m
+//  HelmiTests
+//
+//  Created by markus on 8.2.2014.
+//  Copyright (c) 2014 Markus Kauppila. All rights reserved.
+//
+
+#import "Kiwi.h"
+
+#import "User.h"
+
+SPEC_BEGIN(UserSpec)
+
+describe(@"User", ^{
+    it(@"has correct email address", ^{
+    });
+});
+
+SPEC_END
+
