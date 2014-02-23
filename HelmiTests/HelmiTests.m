c5effc936065ec4bbf3e374ff4d9b315b7b20453
Markus Kauppila <markus.kauppila@gmail.com>
2014-02-23 17:18:21 +0200

    Test Kiwi installation

--- HelmiTests/HelmiTests.m
+++ HelmiTests/HelmiTests.m
@@ -6,29 +6,17 @@
 //  Copyright (c) 2014 Markus Kauppila. All rights reserved.
 //
 
-#import <XCTest/XCTest.h>
+#import "Kiwi.h"
 
-@interface HelmiTests : XCTestCase
+SPEC_BEGIN(MathSpec)
 
-@end
+describe(@"Math", ^{
+    it(@"is pretty cool", ^{
+        NSUInteger a = 16;
+        NSUInteger b = 26;
+        [[theValue(a + b) should] equal:theValue(42)];
+    });
+});
 
-@implementation HelmiTests
+SPEC_END
 
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
-}
-
-@end
