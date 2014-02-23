10a0a1e245f62d62aae9d0cc89a49eef78046c6b
Markus Kauppila <markus.kauppila@gmail.com>
2014-02-23 18:22:34 +0200

    Add specs to UserSpec
    Verify the correct email address after creating User object.

--- HelmiTests/UserSpec.m
+++ HelmiTests/UserSpec.m
@@ -6,14 +6,36 @@
 //  Copyright (c) 2014 Markus Kauppila. All rights reserved.
 //
 
-#import "Kiwi.h"
+#import <Kiwi.h>
 
 #import "User.h"
+
+NSDictionary *LoadTestData(NSString *fileName)
+{
+    NSString *pathToTestData = [[NSBundle bundleWithIdentifier:@"UnitTest"]
+                                pathForResource:fileName ofType:@"json"];
+    NSData *jsonData = [NSData dataWithContentsOfFile:pathToTestData];
+    return [NSJSONSerialization JSONObjectWithData:jsonData
+                                           options:kNilOptions
+                                             error:NULL];
+}
 
 SPEC_BEGIN(UserSpec)
 
 describe(@"User", ^{
+    __block User *user = nil;
+    
+    beforeEach(^{
+        NSDictionary *userInfo = LoadTestData(@"login");
+        user = [[User alloc] initWithUserInfo:userInfo];
+    });
+    
+    afterEach(^{
+        user = nil;
+    });
+    
     it(@"has correct email address", ^{
+        [[[user emailAddress] should] equal:@"markus.kauppila@gmail.com"];
     });
 });
 
