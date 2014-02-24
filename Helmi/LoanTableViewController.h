b6c648f02d520fdb54c014408ae6bb4d926ca873
Markus Kauppila <markus.kauppila@gmail.com>
2014-02-24 18:55:37 +0200

    Add LoanTableViewController

---
+++ Helmi/LoanTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoanTableViewController.h
+//  Helmi
+//
+//  Created by markus on 24.2.2014.
+//  Copyright (c) 2014 Markus Kauppila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoanTableViewController : UITableViewController
+
+@end
