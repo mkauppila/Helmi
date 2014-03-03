dd7b716beb6ab1faa93a7e6aed1c1153e27cdfce
Markus Kauppila <markus.kauppila@gmail.com>
2014-03-03 14:24:57 +0200

    Add LoanableItem model
    The model represents all the items that are loanable.

---
+++ Helmi/LoanableItem.h
@@ -0,0 +1,13 @@
+//
+//  Loan.h
+//  Helmi
+//
+//  Created by markus on 3.3.2014.
+//  Copyright (c) 2014 Markus Kauppila. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LoanableItem : NSObject
+
+@end
---
+++ Helmi/LoanableItem.m
@@ -0,0 +1,13 @@
+//
+//  Loan.m
+//  Helmi
+//
+//  Created by markus on 3.3.2014.
+//  Copyright (c) 2014 Markus Kauppila. All rights reserved.
+//
+
+#import "LoanableItem.h"
+
+@implementation LoanableItem
+
+@end
