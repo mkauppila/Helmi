eff38bca7eb7257f6bad6a2a608d453e699e171f
Markus Kauppila <markus.kauppila@gmail.com>
2014-03-03 08:43:11 +0200

    Add initial LoanViewModel

---
+++ Helmi/LoanViewModel.h
@@ -0,0 +1,13 @@
+//
+//  LoanViewModel.h
+//  Helmi
+//
+//  Created by markus on 3.3.2014.
+//  Copyright (c) 2014 Markus Kauppila. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LoanViewModel : NSObject
+
+@end
---
+++ Helmi/LoanViewModel.m
@@ -0,0 +1,13 @@
+//
+//  LoanViewModel.m
+//  Helmi
+//
+//  Created by markus on 3.3.2014.
+//  Copyright (c) 2014 Markus Kauppila. All rights reserved.
+//
+
+#import "LoanViewModel.h"
+
+@implementation LoanViewModel
+
+@end
