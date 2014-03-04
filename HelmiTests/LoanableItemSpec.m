89aa0e1f944da3cd14eb39e71132dfbcdeca56f8
Markus Kauppila <markus.kauppila@gmail.com>
2014-03-04 09:57:17 +0200

    Add LoanableItemSpec

---
+++ HelmiTests/LoanableItemSpec.m
@@ -0,0 +1,17 @@
+//
+//  LoanableItemSpec.m
+//  Helmi
+//
+//  Created by markus on 4.3.2014.
+//  Copyright (c) 2014 Markus Kauppila. All rights reserved.
+//
+
+#import <Kiwi.h>
+
+#import "SpecHelper.h"
+
+SPEC_BEGIN(LoanableItemSpec)
+
+
+
+SPEC_END
