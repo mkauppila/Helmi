3b3274a38f824e44d9a3c5971b4a84b688bd4199
Markus Kauppila <markus.kauppila@gmail.com>
2014-03-04 10:14:27 +0200

    Create full spec for LoanableItemSpec

--- HelmiTests/LoanableItemSpec.m
+++ HelmiTests/LoanableItemSpec.m
@@ -19,6 +19,9 @@ describe(@"Loanble item", ^{
     
     beforeEach(^{
         item = [[LoanableItem alloc] initWithIdentifier:@"123456"];
+        NSDictionary *itemInfo = LoadTestData(@"item.json");
+
+        [item loadInformationFrom:itemInfo];
     });
     
     afterEach(^{
@@ -27,6 +30,35 @@ describe(@"Loanble item", ^{
     
     it(@"it needs to have identifier", ^{
         [[[item identifier] should] beNonNil];
+    });
+    
+    it(@"is due to March, 26th 2014", ^{
+        NSCalendarUnit dateComponents =  NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay;
+        NSDateComponents *components = [[NSCalendar currentCalendar] components:dateComponents
+                                                                       fromDate:[item dueDate]];
+        [[theValue([components day]) should] equal:theValue(26)];
+        [[theValue([components month]) should] equal:theValue(3)];
+        [[theValue([components year]) should] equal:theValue(2014)];
+    });
+    
+    it(@"is authored by Frank Herbert", ^{
+        [[[item authorFirstName] should] equal:@"Frank"];
+        [[[item authorFirstName] should] equal:@"Herbert"];
+    });
+    
+    it(@"has title 'Dune Messiah'", ^{
+        [[[item title] should] equal:@"Dune Messiah"];
+    });
+    
+    it(@"it has circulation status of charged", ^{
+        [[[item circulationStatus] should] equal:@"charged"];
+    });
+    
+    // This doesn't worry about the due date, since overdue status is
+    // retrieved by from circulation status. Thus this doesn't make the
+    // test flaky
+    it(@"is not overdue yet", ^{
+        [[theValue([item isOverdue]) should] beFalse];
     });
 });
 
