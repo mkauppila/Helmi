881b5ea5f6dfad3f71dd8305f6c7822a82649d22
Markus Kauppila <markus.kauppila@gmail.com>
2014-03-06 22:43:28 +0200

    Add table view cell called LoanCell

---
+++ LoanCell.h
@@ -0,0 +1,15 @@
+//
+//  LoanCell.h
+//  Helmi
+//
+//  Created by markus on 6.3.2014.
+//  Copyright (c) 2014 Markus Kauppila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class  RACDisposable;
+
+@interface LoanCell : UITableViewCell
+@property (nonatomic, strong) RACDisposable *update;
+@end
---
+++ LoanCell.m
@@ -0,0 +1,31 @@
+//
+//  LoanCell.m
+//  Helmi
+//
+//  Created by markus on 6.3.2014.
+//  Copyright (c) 2014 Markus Kauppila. All rights reserved.
+//
+
+#import "LoanCell.h"
+
+#import <ReactiveCocoa.h>
+#import <libextobjc/EXTScope.h>
+
+@implementation LoanCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+}
+
+
+
+@end
