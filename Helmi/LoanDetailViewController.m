4b916e4727dbed72dbdf53edefd9591c3f444546
Markus Kauppila <markus.kauppila@gmail.com>
2014-03-08 18:50:58 +0200

    Add LoanDetailViewModel to corresponding controller

--- Helmi/LoanDetailViewController.m
+++ Helmi/LoanDetailViewController.m
@@ -8,16 +8,29 @@
 
 #import "LoanDetailViewController.h"
 
+#import "LoanDetailViewModel.h"
 #import "LoanableItem.h"
 
 @interface LoanDetailViewController ()
+@property (nonatomic, strong) LoanDetailViewModel *detailViewModel;
 @end
 
 @implementation LoanDetailViewController
 
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super init];
+    if (self) {
+        _detailViewModel = [LoanDetailViewModel new];
+    }
+    return self;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    [self.detailViewModel setItem:self.loanableItem];
     
     self.title = [self.loanableItem title];
 }
