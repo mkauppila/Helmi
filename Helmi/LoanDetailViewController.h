3df05e60c9de91da0d47b154fe6d554bbb56f6c7
Markus Kauppila <markus.kauppila@gmail.com>
2014-03-08 18:16:30 +0200

    Add LoanDetailViewController
    Gives more detailed view to a loanable item.

---
+++ Helmi/LoanDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoanDetailViewController.h
+//  Helmi
+//
+//  Created by markus on 8.3.2014.
+//  Copyright (c) 2014 Markus Kauppila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoanDetailViewController : UIViewController
+
+@end
---
+++ Helmi/LoanDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  LoanDetailViewController.m
+//  Helmi
+//
+//  Created by markus on 8.3.2014.
+//  Copyright (c) 2014 Markus Kauppila. All rights reserved.
+//
+
+#import "LoanDetailViewController.h"
+
+@interface LoanDetailViewController ()
+
+@end
+
+@implementation LoanDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
