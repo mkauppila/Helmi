9d14cb8b12767aa0865bf9a1eeab9184e7bd935e
Markus Kauppila <markus.kauppila@gmail.com>
2014-03-08 18:40:10 +0200

    Clean up LoanDetailViewController
    Remove the boilerplate code.

--- Helmi/LoanDetailViewController.m
+++ Helmi/LoanDetailViewController.m
@@ -11,32 +11,15 @@
 #import "LoanableItem.h"
 
 @interface LoanDetailViewController ()
-
 @end
 
 @implementation LoanDetailViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
     
     self.title = [self.loanableItem title];
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
