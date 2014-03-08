4ff3b54ea297957da247ba19e7a33e995a155f45
Markus Kauppila <markus.kauppila@gmail.com>
2014-03-08 18:46:23 +0200

    Add LoanDetailViewModel

---
+++ Helmi/LoanDetailViewModel.h
@@ -0,0 +1,13 @@
+//
+//  LoanDetailViewModel.h
+//  Helmi
+//
+//  Created by markus on 8.3.2014.
+//  Copyright (c) 2014 Markus Kauppila. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LoanDetailViewModel : NSObject
+
+@end
---
+++ Helmi/LoanDetailViewModel.m
@@ -0,0 +1,13 @@
+//
+//  LoanDetailViewModel.m
+//  Helmi
+//
+//  Created by markus on 8.3.2014.
+//  Copyright (c) 2014 Markus Kauppila. All rights reserved.
+//
+
+#import "LoanDetailViewModel.h"
+
+@implementation LoanDetailViewModel
+
+@end
